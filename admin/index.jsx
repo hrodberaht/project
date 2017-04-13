979dc0e9281ab89c179702104b078f018267727f
Robert <seedraev@gmail.com>
2017-04-13 19:19:24 +0000

    add routing in react

--- admin/index.jsx
+++ admin/index.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 
 class Home extends React.Component {
     render(){
         return(
-        <h1>React</h1>
+        <div>
+            <ul>
+                <li><Link to="/admin">Home</Link></li>
+                <li><Link to="/admin/products">Products</Link></li>
+            </ul>
+        </div>
         );
     }
 }
 
+class IndexPage extends React.Component {
+    render(){
+        return(<h1>React</h1>);
+    }
+}
 
-render(<Home />, document.getElementById("app"));
+class Products extends React.Component {
+    render(){
+        return(<h1>Products</h1>);
+    }
+}
+
+
+render(
+    <Router>
+        <div>
+            <Home/>
+            <Route exact path="/admin" component={IndexPage} />
+            <Route path="/admin/products" component={Products} />
+        </div>
+    </Router>,
+    document.getElementById("app")
+);
--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ app.use('/public', express.static(path.join(__dirname, '../admin/public')));
 app.use('/public', express.static(path.join(__dirname, '../client/public')));
 
 
-app.get("/", (req, res) => 
+// app.get("/", (req, res) => 
+// {
+//     res.sendFile(path.join(__dirname,'../client/index.html'));
+// });
+app.get("/admin*", (req, res) => 
 {
-    res.sendFile(path.join(__dirname,'../client/index.html'));
-});
-app.get("/admin", (req, res) => 
-{
-    res.sendFile(path.join(__dirname,'../admin/admin.html'));
+    res.sendFile(path.join(__dirname,'../admin/public/admin.html'));
 });
 
 
