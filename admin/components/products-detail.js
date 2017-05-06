9c029f2ed1281685433f728bbba7fed4ed1382a0
Robert <seedraev@gmail.com>
2017-05-06 18:41:31 +0000

    working on product details

--- admin/components/products-detail.js
+++ admin/components/products-detail.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 
 class ProductsDetail extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state ={ 
+            "1":1,
+            "2":2
+            
+        };
+        
+    }
+    
     render(){
+        
+        const id = this.props.match.params.id;
         return(
             <div>
                 <h1>Details</h1>
-                <p>{"id: " + this.props.match.params.id}</p>
+                <p>{"id: " + id}</p>
+                <p>{this.state[id]}</p>
             </div>
         );
     }
--- server/server.js
+++ server/server.js
@@ -7,11 +7,14 @@ const app = express();
 app.use('/public', express.static(path.join(__dirname, '../admin/public')));
 app.use('/public', express.static(path.join(__dirname, '../client/public')));
 
-
+//client page
 app.get("/", (req, res) => 
 {
     res.sendFile(path.join(__dirname,'../client/index.html'));
 });
+
+
+//admin page
 app.get("/admin", (req, res) => 
 {
     res.sendFile(path.join(__dirname,'../admin/public/admin.html'));
@@ -22,10 +25,11 @@ app.get("/admin/*", (req, res) =>
     res.redirect("/admin");
 });
 
+//api 
 app.use("/api", api);
 
 
-
+//server
 app.listen(process.env.PORT, () => 
 {
     console.log('Server start');
